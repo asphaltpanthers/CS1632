330dc4b6620373844f17dbab09267f3588b42105
diser <diser@asphaltpanthers.com>
2019-05-30 22:05:23 -0400

    Get visual studio tests working.

--- examples/Lecture11/IS2545Lecture11NetBeans/src/BarnyardWithDependencyInjectionAndPolymorphism/Barnyard.java
+++ examples/Lecture11/IS2545Lecture11NetBeans/src/BarnyardWithDependencyInjectionAndPolymorphism/Barnyard.java
@@ -12,6 +12,21 @@ import java.util.ArrayList;
  * @author AsphaltPanthers
  */
 public class Barnyard {
+    /*This is an example of an application with dependency injection and polymorphism. We still are injecting the dependencies as parameters,
+         * but the parameter is a list of an abstract data type named IAnimal. IAnimal is an interface with one method that need overriding,
+         * Vocalize(). This is an improvement for two reasons:
+         * 
+         * 1. Testability
+         * 
+         * Now when we create mocks (as seen in Barnyard.WithDependencyInjectionAndPolymorphism.Tests) we only need to create a mock for one data
+         * type instead of three.
+         * 
+         * 2. Extensibility
+         * 
+         * With this implementation it becomes much easier to add animals to the barnyard. In fact, we can do it without making any changes to
+         * this code. This code can support barnyards made up of any number or types of animals. This results in less code overhead and simpler,
+         * easier to understand solutions that are better tested.
+         */
     private final ArrayList<IAnimal> _animals;
     
     public Barnyard(ArrayList<IAnimal> animals) {
